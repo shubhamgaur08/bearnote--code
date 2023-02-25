1cc12a152db8ef08974a026bd64dda60f9c85138
shubham gound <73541878+shubhamgaur08@users.noreply.github.com>
2023-02-25 20:20:26 +0530

    3rd

---
+++ back-end/index.js
@@ -0,0 +1,21 @@
+const connectToMongo = require('./db');
+const express = require('express')
+var cors = require('cors') 
+
+connectToMongo();
+const app = express()
+const port = 5000
+
+app.use(cors({
+  origin: '*'
+}));
+app.use(express.json())
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+
+app.listen(port, () => {
+  console.log(`BearNote listening at http://localhost:${port}`)
+})
